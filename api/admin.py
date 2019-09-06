93851a7124057372ad993d6d09abe1a20c7678a1
GiancarloFusiello <g.fusiello@elifesciences.org>
2019-09-06 16:37:37 +0100

    remove admins.py

--- api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from api.models import Category, Journal
-
-
-# Register your models here to view them in the admin panel.
-admin.site.register(Category)
-admin.site.register(Journal)
